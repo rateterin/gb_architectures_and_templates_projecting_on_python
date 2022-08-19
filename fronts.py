c810bb0fb516fa4cb84e58fa6a31d49c1bb397d6
rateterin <rateterin@yandex.ru>
2022-08-19 16:18:07 +0300

    fix requests logging in front-controller

--- fronts.py
+++ fronts.py
@@ -1,15 +1,20 @@
 import logging
+import os
 from datetime import datetime
 from settings import TEMPLATES_FOLDER
 
 
+if not os.path.isdir("logs"):
+    os.mkdir("logs")
+
 logging.basicConfig()
 request_log = logging.getLogger("request")
+request_log.setLevel(logging.INFO)
 request_log.addHandler(logging.FileHandler("logs/request.log"))
 
 
 def request_log_front(request):
-    request_log.info(f"{datetime.now()} :: {str(request)}")
+    request_log.info(f"{datetime.now()} :: {request}")
 
 
 def static(request):
